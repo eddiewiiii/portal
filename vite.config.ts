120e601c5b4e368faf7090ecb8b8ee3e58975290
Eddie <34086760+eddiewiiii@users.noreply.github.com>
2026-07-23 20:18:56 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { viteSingleFile } from "vite-plugin-singlefile";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -13,14 +12,5 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-  },
-  build: {
-    assetsInlineLimit: 100000000,
-    cssCodeSplit: false,
-    rollupOptions: {
-      output: {
-        inlineDynamicImports: true,
-      },
-    },
   },
 });
